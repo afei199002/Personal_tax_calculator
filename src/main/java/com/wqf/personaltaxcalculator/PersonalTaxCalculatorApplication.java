e148e60210148abe449d278a7b5b1ea604b085cf
qifei.wang <afei199002@gmail.com>
2019-01-10 14:28:47 +0800

    init

---
+++ src/main/java/com/wqf/personaltaxcalculator/PersonalTaxCalculatorApplication.java
@@ -0,0 +1,14 @@
+package com.wqf.personaltaxcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTaxCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTaxCalculatorApplication.class, args);
+	}
+
+}
+
