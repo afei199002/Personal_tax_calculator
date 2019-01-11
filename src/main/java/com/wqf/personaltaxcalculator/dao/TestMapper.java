fa78cb975117e725b701363eaba7aebff6cf3119
qifei.wang <afei199002@gmail.com>
2019-01-11 18:07:42 +0800

    init

--- src/main/java/com/wqf/personaltaxcalculator/dao/TestMapper.java
+++ src/main/java/com/wqf/personaltaxcalculator/dao/TestMapper.java
@@ -1,5 +1,10 @@
 package com.wqf.personaltaxcalculator.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper
 public interface TestMapper {
-    void test();
+    Object test();
 }
